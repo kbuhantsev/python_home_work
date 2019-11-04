2337b38d174c10dc1a71257ecf64f193348f8b51
Konstantyn <k.buhantsev@gmail.com>
2019-11-04 12:03:51 +0200

    Create DataTime.py

---
+++ Наработки/DataTime.py
@@ -0,0 +1,7 @@
+import datetime
+
+a = datetime.datetime.today()
+print(a)  # datetime.datetime(2017, 4, 5, 0, 16, 54, 989663)
+
+b = datetime.datetime.now()
+print(b)  # datetime.datetime(2017, 4, 5, 0, 17, 8, 24239)
