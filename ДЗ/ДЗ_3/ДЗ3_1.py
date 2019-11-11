dfee9c7d82227501ec68dfd0ae3a6a8488254245
Konstantyn <k.buhantsev@gmail.com>
2019-11-11 22:15:17 +0200

    Create ДЗ3_1.py

---
+++ ДЗ/ДЗ_3/ДЗ3_1.py
@@ -0,0 +1,32 @@
+def copy_file(file_name="", copy_name=None):
+    try:
+        input_file = open(file_name, "r")
+    except FileNotFoundError:
+        print("Не удалось открыть входной файл!")
+        return None
+
+    if copy_name is None:
+        file_number = 1
+        while True:
+            try:
+                new_file_name = file_name.split(".")[0] + str(file_number) \
+                                + "." + file_name.split(".")[1]
+                output_file = open(new_file_name)
+            except FileNotFoundError:
+                copy_name = new_file_name
+                break
+            file_number += 1
+
+    try:
+        output_file = open(copy_name, "w")
+        for line in input_file:
+            output_file.write(line)
+            output_file.flush()
+    except FileNotFoundError:
+        print("Не удалось найти файл назначения!")
+    finally:
+        output_file.close()
+
+
+copy_file("cache.txt")
+copy_file("cache.txt", "test.txt")
