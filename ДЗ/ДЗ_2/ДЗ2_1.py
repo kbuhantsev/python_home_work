e1de023e0c1aa0484f4aaa770c2b67e08b801e89
Konstantyn <k.buhantsev@gmail.com>
2019-11-11 12:08:53 +0200

    Update ДЗ2_1.py

--- ДЗ/ДЗ_2/ДЗ2_1.py
+++ ДЗ/ДЗ_2/ДЗ2_1.py
@@ -1,4 +1,4 @@
-from functools import wraps
+from functools import wraps, lru_cache
 
 
 def my_lru_cache(maxsize=None):
@@ -10,19 +10,31 @@ def my_lru_cache(maxsize=None):
             key = args + tuple(sorted(kwargs.items()))
             if key in wrapped.cache:  # Нашлось в кэше, возвращаем
                 wrapped.hits += 1
-                return wrapped.cache.get(key)
+                wrapped.list_keys.append(wrapped.list_keys.pop(wrapped.list_keys.index(key)))
             else:
                 wrapped.misses += 1
-                if (maxsize is None) or (len(wrapped.cache) < maxsize):  # Место в кэшэ есть, добавляем
-                    wrapped.cache[key] = func(*args, **kwargs)
-                    return wrapped.cache[key]
-                else:  # Места в кэшэ нет, очищаем
-                    sorted(wrapped.cache.items(), key=lambda item: item[1])
-                    wrapped.cache[0] = func(*args, **kwargs)
-                    return wrapped.cache[0]
+                wrapped.list_keys.append(key)
+                wrapped.cache[key] = func(*args, **kwargs)
+                if maxsize is not None and len(wrapped.cache) > maxsize:
+                    wrapped.cache.pop(wrapped.list_keys.pop(0))
 
+            return wrapped.cache[key]
+
+        def cache_info():
+            print("CacheInfo(hits={hits}, misses={misses}, maxsize={maxsize}, curr_size={curr_size})". \
+                  format(hits=wrapped.hits, misses=wrapped.misses,
+                         maxsize=maxsize, curr_size=len(wrapped.cache)))
+
+        def cache_clear():
+            wrapped.cache = {}
+            wrapped.list_keys = []
+            wrapped.hits = wrapped.misses = 0
+
+        wrapped.cache_info = cache_info
+        wrapped.cache_clear = cache_clear
         wrapped.maxsize = maxsize
         wrapped.cache = {}
+        wrapped.list_keys = []
         wrapped.hits = wrapped.misses = 0
 
         return wrapped
@@ -30,17 +42,29 @@ def my_lru_cache(maxsize=None):
     return my_decorator
 
 
-@my_lru_cache(None)
+@my_lru_cache(3)
 def fib(n):
-
     if n < 2:
         return n
     return fib(n - 1) + fib(n - 2)
 
 
 print([fib(n) for n in range(16)])
-print("CacheInfo(hits={hits}, misses={misses}, maxsize={maxsize}, curr_size={curr_size})".\
-      format(hits=fib.hits, misses=fib.misses,
-             maxsize=fib.maxsize, curr_size=len(fib.cache)))
+fib.cache_info()
+
+print("*******************************************************************")
 
 
+@lru_cache(3)
+def fib2(n):
+    if n < 2:
+        return n
+    return fib2(n - 1) + fib2(n - 2)
+
+
+print([fib2(n) for n in range(16)])
+print(fib2.cache_info())
+
+print("************************clear cache**************************************")
+fib.cache_clear()
+fib.cache_info()
